e53d778edc8faf1bc2354683689830ad20e9b8c8
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-08-06 21:02:28 -0500

    clases 06/08/2024

---
+++ Palabra.py
@@ -0,0 +1,8 @@
+#definicion de una funcion sin parametros
+def MiMensaje():
+    print("learning to create function")
+MiMensaje()
+
+def MiMensajeConNombr(name):
+    print(name, "esta aprendiendo en python ")
+    
